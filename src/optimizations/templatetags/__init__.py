93d93e12615be36a14a8cd267b0ffa928372e301
Dave Hall <dave@etianen.com>
2011-09-28 11:47:12 +0100

    Working on simplifying template tags.

--- src/optimizations/templatetags/__init__.py
+++ src/optimizations/templatetags/__init__.py
@@ -1,1 +1,84 @@
 """Template tags used by django-optimizations."""
+
+import re
+from functools import wraps
+
+from django import template
+
+
+RE_KWARG = re.compile(u"([a-z][a-z0-9_]*)=(.*)", re.IGNORECASE)
+
+
+def parse_token(token):
+    """Parses the given token into a tuple of (args, kwargs and alias)."""
+    parts = token.split_contents()[1:]
+    args = []
+    kwargs = {}
+    # Parse the alias.
+    if len(parts) >= 2 and parts[-2] == "as":
+        alias = parts[-1]
+        parts = parts[:-2]
+    else:
+        alias = None
+    # Parse the args.
+    parts_iter = iter(parts)
+    for part in parts_iter:
+        kwarg_match = RE_KWARG.match(part)
+        if kwarg_match:
+            kwargs[kwarg_match.group(1)] = template.Variable(kwarg_match.group(2))
+        else:
+            if kwargs:
+                raise template.TemplateSyntaxError("Keyword arguments cannot follow position arguments")
+            args.append(template.Variable(part))
+    # All done!
+    return args, kwargs, alias
+
+
+class ParameterNode(template.Node):
+
+    """A node for a paramter tag."""
+    
+    def __init__(self, takes_context, func, args, kwargs, alias):
+        """Initializes the parameter node."""
+        self._takes_context = takes_context
+        self._func = func
+        self._args = args
+        self._kwargs = kwargs
+        self._alias = alias
+        
+    def render(self, context):
+        """Renders the parameter node."""
+        # Resolve all variables.
+        args = [arg.resolve(context) for arg in self._args]
+        kwargs = {
+            name: value.resolve(context)
+            for name, value
+            in self._kwargs.iteritems()
+        }
+        # Add in the context.
+        if self._takes_context:
+            args.insert(0, context)
+        # Run the tag.
+        result = self._func(*args, **kwargs)
+        # Alias if required.
+        if self._alias:
+            context[self._alias] = result
+            return ""
+        # Render the result.
+        return unicode(result)
+    
+    
+def parameter_tag(takes_context=False):
+    """A decorator for a function that should be converted to a parameter tag."""
+    def decorator(func):
+        @wraps(func)
+        def compiler(parser, token):
+            args, kwargs, alias = parse_token(token)
+            return ParameterNode(takes_context, func, args, kwargs, alias)
+        return compiler
+    # Adapt to no arguments.
+    if callable(takes_context):
+        func = takes_context
+        takes_context = False
+        return decorator(func)
+    return decorator
--- src/optimizations/templatetags/assets.py
+++ src/optimizations/templatetags/assets.py
@@ -1,84 +1,45 @@
 """Template tags used for optimizing assets."""
 
-import re
-
 from django import template
 
 from optimizations.thumbnailcache import default_thumbnail_cache
+from optimizations.templatetags import parameter_tag
 
 
 register = template.Library()
 
 
-RE_KWARG = re.compile(u"([a-z][a-z0-9_]*)=(.*)", re.IGNORECASE)
+class ThumbnailRenderer(object):
 
-
-def parse_token(token):
-    """Parses the given token into a tuple of (args, kwargs and alias)."""
-    parts = token.split_contents()[1:]
-    args = []
-    kwargs = {}
-    # Parse the alias.
-    if len(parts) >= 2 and parts[-2] == "as":
-        alias = parts[-1]
-        parts = parts[:-2]
-    else:
-        alias = None
-    # Parse the args.
-    parts_iter = iter(parts)
-    for part in parts_iter:
-        kwarg_match = RE_KWARG.match(part)
-        if kwarg_match:
-            kwargs[kwarg_match.group(1)] = template.Variable(kwarg_match.group(2))
-        else:
-            if kwargs:
-                raise template.TemplateSyntaxError("Keyword arguments cannot follow position arguments")
-            args.append(template.Variable(part))
-    # All done!
-    return args, kwargs, alias
-
-
-class ThumbnailNode(template.Node):
-
-    """A node used to render a thumbnail."""
+    """Renders a thumbnail object."""
     
-    def __init__(self, alias, src, **attrs):
-        """Initializes the thumbnail node."""
-        self._alias = alias
-        self._thumbnail_var = src
-        self._attr_vars = attrs
+    def __init__(self, thumbnail, alt, attrs):
+        """Initializes the renderer."""
+        self._thumbnail = thumbnail
+        self.width = thumbnail.width
+        self.height = thumbnail.height
+        self.url = thumbnail.url
+        self.alt = alt
+        self.attrs = attrs
         
-    def render(self, context):
-        """Renders the thumbnail node."""
-        # Resolve the variables.
-        thumbnail_asset = self._thumbnail_var.resolve(context)
-        attrs = {
-            name: variable.resolve(context)
-            for name, variable
-            in self._attr_vars.iteritems()
-        }
-        # Create the thumbnail.
-        thumbnail = default_thumbnail_cache.get_thumbnail(
-            thumbnail_asset,
-            width = attrs.pop("width", None),
-            height = attrs.pop("height", None),
-        )
-        # Response to alias.
-        if self._alias:
-            context[self._alias] = thumbnail
-            return ""
-        # Render the thumbnail.
+    def __unicode__(self):
+        """Renders the thumbnail."""
         return template.loader.render_to_string("assets/img.html", {
-            "url": thumbnail.url,
-            "width": thumbnail.width,
-            "height": thumbnail.height,
-            "alt": attrs.pop("alt", ""),
-            "attrs": attrs,
+            "url": self.url,
+            "width": self.width,
+            "height": self.height,
+            "alt": self.alt,
+            "attrs": self.attrs,
         })
     
 
 @register.tag
-def img(parser, token):
+@parameter_tag
+def img(src, width=None, height=None, alt="", **attrs):
     """Renders an image tag."""
-    args, kwargs, alias = parse_token(token)
-    return ThumbnailNode(alias, *args, **kwargs)
+    thumbnail = default_thumbnail_cache.get_thumbnail(
+        src,
+        width = width,
+        height = height,
+    )
+    return ThumbnailRenderer(thumbnail, alt, attrs)
