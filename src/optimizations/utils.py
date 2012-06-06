6e3e5955e3d45eabe12b643c60b137ab41c66f68
Dave Hall <dave@etianen.com>
2012-06-06 16:40:24 +0100

    Fixing issue with cache loading in Django 1.3

--- src/optimizations/utils.py
+++ src/optimizations/utils.py
@@ -7,7 +7,7 @@ def resolve_namespaced_cache(name):
     """Finds the best-matching named cache that exists."""
     try:
         return get_cache(name)
-    except InvalidCacheBackendError:
+    except (InvalidCacheBackendError, ValueError):
         if "." in name:
             return resolve_namespaced_cache(name.rsplit(".", 1)[0])
         return default_cache
