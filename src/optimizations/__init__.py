88c2c7f2afe0512539f4debf1639f4a491fc4722
Dave Hall <dave@etianen.com>
2012-07-30 11:16:02 +0100

    Pulling get_url into top-level namespace.

--- src/optimizations/__init__.py
+++ src/optimizations/__init__.py
@@ -7,9 +7,11 @@ Developed by Dave Hall.
 """
 
 from optimizations.propertycache import cached_property
+from optimizations.assetcache import default_asset_cache
 from optimizations.thumbnailcache import default_thumbnail_cache
 from optimizations.stylesheetcache import default_stylesheet_cache
 from optimizations.javascriptcache import default_javascript_cache
 
 
+get_url = default_asset_cache.get_url
 get_thumbnail = default_thumbnail_cache.get_thumbnail
