8cf45f1cb3ba72a2bd8bf7d4db5161e518cf803d
Dave Hall <dave@etianen.com>
2014-09-14 16:18:57 +0100

    Fixing javascript compile error

--- src/tests/test_optimizations/tests/test_javascriptcompiler.py
+++ src/tests/test_optimizations/tests/test_javascriptcompiler.py
@@ -8,4 +8,4 @@ from optimizations.javascriptcompiler import default_javascript_compiler
 class JavascriptCompilerTest(TestCase):
     
     def testJavascriptCompiler(self):
-        self.assertEqual(default_javascript_compiler.compile("function(){var foo = 'foo';}"), 'function(){var a="foo"};')
+        self.assertEqual(default_javascript_compiler.compile("function(){var foo = 'foo';}"), b'function(){var a="foo"};')
